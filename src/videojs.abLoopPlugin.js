dfd84c24451b35803518b2c8d216824e6e02519a
phughson <phughson@mybpos.net>
2016-02-19 12:51:19 +0000

    Simplified API setup - now replaces reference Also added sample HTML file and updated readme version bumped to 0.2.0

--- src/videojs.abLoopPlugin.js
+++ src/videojs.abLoopPlugin.js
@@ -10,25 +10,17 @@
   } else {
     factory(root, root.videojs);
   }
-
 })(window, function(window, videojs) {
 	"use strict";
 
-	videojs.plugin('abLoopPlugin', function (initialOptions) {
+	var version = "0.2.0";
+	var abLoopPlugin = function (initialOptions) {
 		var player = this;
+
+		//contains options, allowing them to be set at runtime
+		var opts = {};		
 		
-		//plugin instance - same object
-		var pluginName = 'abLoopPlugin';	
-		var plugin = player[pluginName]; //already assisgned, a reference to this function
-		plugin.VERSION = '0.1.0';
-	
-		//api is a per player interface
-		var apiName = pluginName + 'API';    //this is the interface for controlling the loop on each player
-		player[apiName] = player[apiName] || {}; 	
-		var api = player[apiName];   
-		api.options = api.options || {};
-		var opts = api.options;		
-		
+		//utility functions
 		var isNumber = function (n) {
 			return !isNaN(parseFloat(n)) && isFinite(n);
 		}
@@ -42,26 +34,34 @@
 		
 		//check that the options make sense. This is called on timeupdate, before the main logic.
 		//so nonsense options will persist as long as time does not update
-		var validateOptions = function(){
 		
-			//start defaults to 0;
-			if (opts.start == undefined || !isNumber(opts.start) || opts.start < 0){opts.start = 0;}
-			
-			//end defaults to duration, or start + 1 if it's not set
-			//setting it to false 
-			var duration = player.duration();
-			if (duration){
-				if ( opts.end ==undefined|| opts.end ===false || !isNumber(opts.end) ||opts.end < opts.start || opts.end > duration  ){
-					opts.end = duration;
-				}	
-			} else {
-				//allow undefined end to default to false - which will become duration hopefully
-				if (opts.end == undefined){opts.end = false; }
-				if (opts.end !== false && (!isNumber(opts.end) || opts.end < opts.start)){
-					opts.end = opts.start + 1;
+		var validateStart = function(s){
+			if (s===false){return s;}
+			if (isNumber(s) && s>= 0){return s;} 
+			return 0;
+		}
+		var validateEnd = function(x){
+			if (x===false){return x;}
+			if (isNumber(x)){
+				var duration = player.duration();
+				if (duration == undefined || duration === 0){
+					if ( x>= 0 ){return x;} 
+				} else {
+					if ( x>= 0 && x <= duration){
+						return x;
+					} else if ( x> duration){
+						return duration;
+					}
 				}
 			}
-
+			return false;// by default, loop to end of video
+		}		
+		
+		var validateOptions = function(){
+			
+			opts.start = validateStart(opts.start);
+			opts.end = validateEnd(opts.end);
+			
 			//clean up booleans, forcing them to be actual booleans
 			booleanOptions.forEach(function(optName){
 				opts[optName] = opts[optName] ? true : false;
@@ -97,12 +97,11 @@
 			}
 			//invoke callback if set
 			if (isFunction(api.onLoopCallBack)){
-				api.onLoopCallBack(api,opts,player);
+				api.onLoopCallBack(opts,api,player);
 			}
 			return api;
 		}
 
-		
 		var goToEndOfLoop = function(){
 			player.currentTime(opts.end);
 			return api;
@@ -137,50 +136,63 @@
 			return opts; 
 		}*/
 		
-		//functions to expose as API 
-		api.validateOptions = validateOptions;
-		api.resetToInitialOptions = setOptionsToInitialOptions;
-		api.setOptions = setOptions;
-		//api.getOptions = getOptions;
-		api.goToStart = goToStartOfLoop;
-		api.goToEnd = goToEndOfLoop;	
-		api.setStart = setStartOfLoop;
-		api.setEnd = setEndOfLoop;
-		api.enable = enableLoop;
-		api.disable = disableLoop;
-		api.toggle = toggleLoop;
-		api.togglePauseOnLoop = togglePauseOnLoop;
-		
-		//sample callback function - called when the loop happens
-		api.onLoopCallBack = function(api,opts,player){
-			//console.log("Looping back to " + opts.start);
-		}
-		api.player = player;
-		
-		//main logic
+		//check if we need to go to start of loop
 		var checkABLoop = function(e){
-			//console.log("chekcing loop");
-			if (!opts.enabled){return false;}
-			var curTime = player.currentTime();
-			if (opts.end === false){opts.end=player.duration();}
-			validateOptions();
+			if (!opts.enabled){return false;}   //don't check if not enabled
 			
-			if (opts.start === opts.end){player.pause();}
-			if (opts.moveToStartIfBeforeStart && curTime < opts.start){
-				goToStartOfLoop();
-			} else if (curTime >= opts.end){
+			validateOptions();
+			var curTime = player.currentTime();
+			var endTime = (opts.end === false) ? player.duration() : opts.end;
+			var startTime = (opts.start === false) ? 0 : opts.start;
+			
+			if (startTime === endTime){
+				player.pause();				//to save contant looping behaviour
+			} else if (curTime < startTime && opts.moveToStartIfBeforeStart){
+				goToStartOfLoop();	
+			} else if (curTime >= endTime){
 				//use a margin of one just in case time has skipped a bit past
-				if ((curTime - opts.end) < 1 || opts.moveToStartIfAfterEnd){
+				if ((curTime - endTime) < 1 || opts.moveToStartIfAfterEnd){
 					goToStartOfLoop();
 				}
 			}
 		};
 		
-		//get options
+		//functions etc to expose as API 
+		var api = {
+			options: opts,
+			validateOptions:validateOptions,
+			resetToInitialOptions:setOptionsToInitialOptions,
+			setOptions: setOptions,
+			//getOptions: getOptions,
+			goToStart: goToStartOfLoop,
+			goToEnd: goToEndOfLoop,	
+			setStart: setStartOfLoop,
+			setEnd: setEndOfLoop,
+			enable: enableLoop,
+			disable: disableLoop,
+			toggle: toggleLoop,
+			togglePauseOnLoop: togglePauseOnLoop,
+			onLoopCallBack: function(opts,api,player){
+				//console.log("Looping back to %s sec on %s",opts.start, player.currentSrc() );
+			},
+			player: player,
+			version: version,
+			//toString: function(){return JSON.stringify(opts);}
+		};
+		
+		//this replaces the reference created to this function on each player object
+		//with a reference to the api object (created once per invocation of this function)
+		//The functions created in this function and referenced by api will still 
+		//be available via closure		
+		player['abLoopPlugin'] = api;
+		
+		//get options to initial options
 		setOptionsToInitialOptions();		
 		//on timeupdate, check if we need to loop
 		player.on('timeupdate',checkABLoop);
-
-	});
+	};
+	
+	abLoopPlugin.VERSION = version;
+	videojs.plugin('abLoopPlugin', abLoopPlugin);
 
 });
